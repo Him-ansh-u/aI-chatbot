7a44e6e195f78ad85d4d2bae838967037d2e8096
ansh <ansh121sh@gmail.com>
2026-01-09 23:42:02 +0530

    feat(): enable ai message

--- server/src/utils/chat.ut.js
+++ server/src/utils/chat.ut.js
@@ -4,13 +4,12 @@ const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
 export const generateAIResponse = async (contents) => {
   try {
-    // const response = await ai.models.generateContent({
-    //   model: "gemini-2.5-flash",
-    //   contents,
-    // });
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents,
+    });
 
-    // return response?.text || "I couldn't generate a response.";
-    return "I couldn't generate a response.";
+    return response?.text || "I couldn't generate a response.";
   } catch (error) {
     console.error("Gemini AI Error:", error);
     throw new Error("AI generation failed");
